60b4212d35ee223c0d688aaf05be93ab75669201
kar-codes <103388857+kar-codes@users.noreply.github.com>
2022-10-23 12:41:55 -0400

    updates
    js added and updates to html, city can now be search for and provides real data such as weather conditions, temperature and city name.

---
+++ index.js
@@ -0,0 +1,83 @@
+let current = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wedsneday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDay = days[current.getDay()];
+let currentHour = current.getHours();
+let currentMinutes = current.getMinutes();
+
+let dateNow = document.querySelector("#current-date");
+dateNow.innerHTML = ` ${currentDay} ${currentHour}:${currentMinutes}`;
+
+
+function search(city) {
+  let apiKey = "d78c12116349bac4b6411dc36c6b602c"
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function displayWeatherCondition(response) {
+  console.log(response);
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  let temp= document.querySelector("#current-temp").innerHTML = Math.round(response.data.main.temp);
+  let description = document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+ 
+}
+
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit" , handleSubmit);
+
+
+search("New York");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
