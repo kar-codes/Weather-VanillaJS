a6a7c3bd1186eeb6e887e3bf0255c43704ba2ec7
kar-codes <103388857+kar-codes@users.noreply.github.com>
2022-10-27 17:56:37 -0400

    s.o.s
    broke forecast section

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ let days = [
 ];
 
 function search(city) {
-  let apiKey = "d78c12116349bac4b6411dc36c6b602c"
+  let apiKey = "f4b9b3c3f140t6ca1b114f1eo5df8045"
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
@@ -23,29 +23,40 @@ function formatDate(timestamp){
   return `Last updated:${day} ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+
+  let apiKey = "f4b9b3c3f140t6ca1b114f1eo5df8045"
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   console.log(response);
-  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-city").innerHTML = response.data.city;
   let temperatureElement= (document.querySelector("#current-temp").innerHTML =
-  Math.round(response.data.main.temp) + "ºC");
+  Math.round(response.data.temperature.current) + "ºC");
   let iconElement= document.querySelector("#icon");
-  let descriptionElement = document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  let descriptionElement = document.querySelector("#description").innerHTML = response.data.condition.description;
   let dateElement = document.querySelector("#current-date");
   let windElement = document.querySelector("#wind-condition").innerHTML= "wind speed:" + Math.round(response.data.wind.speed) + " km/h";
-  dateElement.innerHTML= formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  dateElement.innerHTML= formatDate(response.data.time * 1000);
+  iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
+  
+  getForecast(response.data.coordinates);
+
 }
 
 
 function displayForecast() {
   let forecastElement= document.querySelector("#forecast");
-  let forecastHtml= `<div class="row">`;
+  let forecast = response.data.daily;
   let forecastDays = ["Thu","Fri","Sat", "Sun", "Mon"];
-  forecastDays.forEach(function(day){
+  forecast.forEach(function(forecastDay){
     forecastHtml=  forecastHtml + `
     <div class="col">
         <div class="weather-forecast-date"> 
-            ${day}
+            ${forecastDay.data.time}
         </div>
             <img src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_11-256.png" alt="" width="40px"/>
         <div class="weather-forecast-temperature">
@@ -58,7 +69,9 @@ function displayForecast() {
 
 forecastHtml= forecastHtml + `</div>`;
 forecastElement.innerHTML= forecastHtml;
+
 }
+
 
 
 function handleSubmit(event) {
@@ -73,7 +86,6 @@ citySearch.addEventListener("submit" , handleSubmit);
 
 
 search("New York");
-displayForecast();
 
 
 
